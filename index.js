119242f00cdac71a082effaa5cce1e10473a250f
kevinIsomMoringa <kevin.isom@student.moringaschool.com>
2022-08-25 23:33:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+const name = ["Guadalupe", "Ollie", "Aki"]
+const event = "surprise"
+
+const writeCards = (nameArray, eventName) => {
+    let newArray = []
+    for (let i = 0; i < nameArray.length; i++) {
+        newArray.push(`Thank you, ${nameArray[i]}, for the wonderful ${eventName} gift!`);
+        //debugger;
+      }
+    //console.log(newArray)
+    return newArray;
+}
+
+
+console.log(writeCards(name,event))
